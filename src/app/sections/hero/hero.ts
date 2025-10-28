a0a1337f8e1b34b0865f9c2f1f6bfc6dba46170f
LorenzoRossini01 <lory.rox.01@gmail.com>
2025-10-28 16:12:41 +0100

    fix: hero title animation

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,6 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-hero',
@@ -9,4 +14,47 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   styleUrls: ['./hero.css'],
   host: { ngSkipHydration: '' }, // ← evita problemi di hydration
 })
-export class Hero {}
+export class Hero implements AfterViewInit {
+  heroSection = viewChild<ElementRef<HTMLDivElement>>('heroSection');
+  ngAfterViewInit(): void {
+    const headings =
+      this.heroSection()?.nativeElement.querySelectorAll('h1,h2');
+
+    headings?.forEach((heading) => {
+      const text = heading.textContent || '';
+      heading.innerHTML = text
+        .split('')
+        .map((char) => {
+          return `<span class="char">${char}</span>`;
+        })
+        .join('');
+    });
+
+    const letters = headings
+      ? Array.from(headings).flatMap((heading) =>
+          Array.from(heading.querySelectorAll('.char'))
+        )
+      : [];
+
+    gsap.set(letters, {
+      y: 0,
+      x: 0,
+      autoAlpha: 0,
+    });
+
+    if (headings) {
+      gsap.from(headings, {
+        y: -50,
+        duration: 1.5,
+        stagger: 0.5,
+      });
+    }
+
+    gsap.to(letters, {
+      autoAlpha: 1,
+      ease: 'power3.out',
+      stagger: 0.05,
+      duration: 1,
+    });
+  }
+}
