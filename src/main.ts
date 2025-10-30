c41ff83d5094fcffe683a7883fa68ecaa6489a3d
LorenzoRossini01 <lory.rox.01@gmail.com>
2025-10-30 19:45:47 +0100

    feat: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { inject } from '@vercel/analytics';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .then(() => {
+    // Inizializza Vercel Analytics dopo il bootstrap
+    inject({ mode: isDevMode() ? 'development' : 'production' });
+  })
+  .catch((err) => console.error(err));
